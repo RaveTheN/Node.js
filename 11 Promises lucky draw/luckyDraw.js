ccb8a54fd493e031cbac1c044c13f5e8b8b2872d
RaveTheN <peppereres@live.it>
2023-03-18 13:29:23 +0100

    es 12

---
+++ 11 Promises lucky draw/luckyDraw.js
@@ -0,0 +1,33 @@
+// The luckyDraw function returns a promise. Create a promise chain where the function is called
+// for each of the players: Joe, Caroline and Sabrina
+
+// Log out the resolved value for each promise and handle any promise rejections in the chain.
+
+const playersArray = ["Joe", "Carolina", "Sabrina"];
+
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
+
+const playRound = () =>
+  playersArray.forEach((el) =>
+    luckyDraw(el)
+      .then(function (result) {
+        console.log(result);
+      })
+      .catch(function (error) {
+        console.error(error);
+      })
+  );
+
+playRound();
---
+++ 12 await the lucky draw results/asyncLuckyDraws.js
@@ -0,0 +1,24 @@
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
+
+const getResults = async () => {
+  try {
+    const result = await playersArray.forEach((player) => luckyDraw(player));
+    console.log(result);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getResults();
